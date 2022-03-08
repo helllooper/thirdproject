e4b301c9ddaa84370bf113eb432f94862d970293
Emad <helllooper@gmail.com>
2022-03-08 21:13:58 +0200

    22th deploy

--- udagram-api/bin/deploy.sh
+++ udagram-api/bin/deploy.sh
@@ -1,4 +1,4 @@
 cd build
 printenv > .env
-eb init thirdproject --region us-east-1  --platform 64bit Amazon Linux 2 v5.5.0 running Node.js 14
+eb init thirdproject --region us-east-1  --platform "Node.js 14 running on 64bit Amazon Linux 2"
 eb deploy Thirdproject-env
