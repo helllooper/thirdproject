039128f5ead2870259aa232475027d48a54257f9
Emad <helllooper@gmail.com>
2022-03-13 01:09:42 +0200

    43th deploy

--- udagram-api/src/server.ts
+++ udagram-api/src/server.ts
@@ -11,9 +11,14 @@ import { V0_FEED_MODELS, V0_USER_MODELS } from "./controllers/v0/model.index";
 (async () => {
   dotenv.config();
 
-  await sequelize.addModels(V0_FEED_MODELS);
-  await sequelize.addModels(V0_USER_MODELS);
-  await sequelize.sync();
+  try {
+    await sequelize.addModels(V0_FEED_MODELS);
+    await sequelize.addModels(V0_USER_MODELS);
+    await sequelize.sync();
+  } catch(error){
+    console.log(error);
+  }
+  
 
   console.log("Database Connected");
 
